3d741cbf8f5cd9dec42d1a39c6be3d3e78a043bf
Washbear10 <vincentbeyer@web.de>
2021-04-05 20:37:15 +0200

    Disallow Landscape orientation

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+      [
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitUp
+      ]
+    );
     return MaterialApp(
         title: 'Flutter Demo',
         home: HomeScreen()
